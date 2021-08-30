f4408f8fd3924553ba579d073f8df62a85554dda
Edi <edi.zeqiri@stud.unibas.ch>
2021-08-30 17:24:11 +0200

    add project

---
+++ BullCowGame-starter-kit 4.26/Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine("Welcome to Bulls Game!");
+    PrintLine("Press Enter to Play!");
+
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+}
